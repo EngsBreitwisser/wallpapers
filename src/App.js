674ff24c07cc8708376082236d23ece704b24664
EngsBreitwisser <engs-brunobreitwisser@camproeal.edu.br>
2024-08-13 18:24:35 -0300

    projeto de wallpapers do Bruno

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'; // Importa React e os hooks a serem utilizados
+import './App.css'; // Importa o CSS
+import logo from './images/logo.png'; // Importa a logo de seu diretório
 
-function App() {
+function App(){
+  const [wallpapers, setWallpaper] = useState([]); // Aqui está sendo declarado um useState
+
+  useEffect(()=>{
+    let url = 'https://picsum.photos/v2/list?page=5&limit=100'; // Definindo a URL da API que vou requisitar as infos
+    fetch(url).then((response)=>response.json()).then((json)=>setWallpaper(json)); // Fazendo a requisição e atualizando o useState com os dados vindos do Json
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <header>
+        <img src={logo} alt="Logo" className="header-logo" /> {/*Aqui está mostrando a logo*/}
       </header>
+      <div className="image-gallery">
+        {wallpapers.map((value)=>{
+          return (
+            <article key={value.id} className="image-container"> {/* Um container para cada imagem*/}
+              <strong className="author-infos">{value.author}</strong> {/* Mostrando o autor da imagem */}
+              <img src={value.download_url} alt={value.author} className="image"/> {/* Mostrando a imagem */}
+              <button className="download-button" onClick={() => window.open(value.download_url)}>Baixar imagem</button> {/* Botão para baixar a imagem (redireciona para uma nova página devido ao método window.open()) */}
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; // Exporta o App
